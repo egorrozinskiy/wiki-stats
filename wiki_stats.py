cf36cc2a0a18f5382e38a214ffda862feef23d68
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-04-10 17:07:33 +0400

    Update wiki_stats.py

--- wiki_stats.py
+++ wiki_stats.py
@@ -7,7 +7,7 @@ import statistics
 from matplotlib import rc
 rc('font', family='Droid Sans', weight='normal', size=14)
 
-
+import numpy
 import matplotlib.pyplot as plt
 
 
@@ -126,6 +126,10 @@ class WikiGraph:
     def middle_count_links_in_article(self):
         middle_counr_links_in_article = [wg.get_number_of_links_from(i) for i in range(wg.get_number_of_pages()) if not wg.is_redirect(i)]
         return '%0.2f'%statistics.mean(middle_counr_links_in_article),'%0.2f'%statistics.stdev(middle_counr_links_in_article)
+
+    def countlinks_to(self, i):
+        countlinks_to = [0 for i in range(self.get_number_of_pages())]
+
 
     def min_count_links_to_article(self):
         countlinks_to = [0 for i in range(self.get_number_of_pages())]
@@ -280,36 +284,18 @@ class WikiGraph:
                     redirects_to[x] += 1
         return '%0.2f'%statistics.mean(redirects_to), '%0.2f'%statistics.stdev(redirects_to)
 
+    
 
-    def hist(fname, data, bins, xlabel, ylabel, title, facecolor='green', alpha=0.5, transparent=True, **kwargs):
-        plt.clf()
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
-        plt.title(title)
-        plt.grid(True)
-        plt.hist(x=data,bins=bins,color=facecolor,label=title,**kwargs)
-        plt.savefig(fname,transparent=transparent)
-        plt.show()
+def hist(fname, data, bins, xlabel, ylabel, title, facecolor='green', alpha=0.5, transparent=True, **kwargs):
+    plt.clf()
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.grid(True)
+    plt.hist(x=data,bins=bins,color=facecolor,label=title,**kwargs)
+    plt.savefig(fname)
+    plt.show()
 
-    def bfs(self,a,b):
-        fired = {a}
-        queue = [a]
-        path = []
-        while queue:
-            current = queue.pop(0)
-            print(self.get_links_from(self.get_id(current)))
-            for neighbour in self.get_links_from(self.get_id(current)):
-                if neighbour not in fired:
-                    fired.add(neighbour)
-                    queue.append(neighbour)
-                    path.append(neighbour)
-                    if neighbour == b:
-                        break
-        return path
-        
-    def path(self,a,b):
-        path = wg.bfs(a,b)
-        return path
 
 
 if __name__ == '__main__':
@@ -334,9 +320,9 @@ if __name__ == '__main__':
     print('Количество статей с максимальным количеством внешних перенаправлений: ', wg.count_articles_with_max_redirects_count())
     print('Статья с наибольшим количеством внешних перенаправлений:', wg.article_with_max_redirects_count())
     print('Среднее количество внешних перенаправлений на статью,среднеквадратичное отклонение :', wg.middle_count_redirects_to_article())
-    hist(fname='Распределение количества ссылок из статьи.png', data=[wg.get_number_of_links_from(i) for i in range(wg._n)], bins=200, xlabel='Количество статей', ylabel="Количество ссылок", title="Распределение количества ссылок из статьи")
-    hist(fname='Распределение количества ссылок на статью.png', data=[countlinks_to[i] for i in range(wg._n)], bins=200, xlabel='Количество статей', ylabel="Количество ссылок", title="Распределение количества ссылок на статью")
-    hist(fname='Распределение количества перенаправлений на статью.png', data=[redirects_to[i] for i in range(wg._n)], bins=50, xlabel='Количество статей', ylabel="Количество ссылок", title="Распределение количества редиректов на статью")
-    hist(fname='Распределение размеров статей.png', data=[wg._sizes[i] for i in range(wg._n)], bins=50, xlabel='Количество статей', ylabel="Количество ссылок", title="Распределение размеров статей")
-    hist(fname='Распределение размеров статей(в логарифмическом масштабе.png', data=[wg._sizes[i] for i in range(wg._n)], bins=50, xlabel='Количество статей', ylabel="Количество ссылок", title="Распределение размеров статей (log)", log=True)
-    wg.path(wg.get_id("Python"), wg.get_id("Список_файловых_систем"))
+    hist('Распределение количества ссылок из статьи.png', [wg.get_number_of_links_from(i) for i in range(wg._n)], 100, 'Количество статей', "Количество ссылок", "Распределение количества ссылок из статьи", range=(0,200))
+    hist('Распределение количества ссылок на статью.png', [[0 for i in range(wg.get_number_of_pages())][i] for i in range(wg._n)], 100, 'Количество статей', "Количество ссылок", "Распределение количества ссылок на статью", range=(0,200))
+    hist('Распределение количества перенаправлений на статью.png', [[0 for i in range(wg.get_number_of_pages())][i] for i in range(wg._n)], 20, 'Количество статей', "Количество ссылок", "Распределение количества редиректов на статью", range=(0,20))
+    hist('Распределение размеров статей.png', [wg._sizes[i] for i in range(wg._n)], 100, 'Количество статей', "Количество ссылок", "Распределение размеров статей", range=(0,100000))
+    hist('Распределение размеров статей(в логарифмическом масштабе.png', numpy.log10([wg._sizes[i] for i in range(wg._n)]), 25, 'Количество статей', "Количество ссылок", "Распределение размеров статей (log)", range=(0,6))
+
